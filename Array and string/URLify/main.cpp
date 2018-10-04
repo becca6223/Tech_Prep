f0dd467204fbf6275155a16379fe55a626d18f57
becca <chowr@purdue.edu>
2018-10-04 07:59:56 -0400

    workinggit add URLify/main.cpp

--- Array and string/URLify/main.cpp
+++ Array and string/URLify/main.cpp
@@ -6,33 +6,22 @@
 #include <string>
 
 //Function Declarations
-void getUserInputs(std::string* input, int* length);
 void insertPercent20(char* input, int length);
 
 int main(int argc, char* argv[]){
-    //std::string str1;
-    //int length;
-
-    //getUserInputs(&str1, &length);
-    char input[20] = "My John Msith";
+    char input[20] = "My John Smith";
     int length = 13;
+
+    std::cout << input << std::endl;
+
     insertPercent20(input, length);
 
     std::cout << input << std::endl;
-    //std::cout << str1 << std::endl;
-    //std::cout << str1[1] << std::endl;
-
 
 
     return EXIT_SUCCESS;
 }
 
-void getUserInputs(std::string* input, int* length) {
-    std::cout << "Give a string: ";
-    std::getline(std::cin, *input);
-    std::cout << "Give true length of the string: ";
-    std::cin >> *length;
-}
 
 void insertPercent20(char* input, int length) {
     int space_cnt = 0;
@@ -41,19 +30,19 @@ void insertPercent20(char* input, int length) {
     for(int i = 0; i < length; i++) {
         if( input[i] == ' ') {
             space_cnt++;
-            std::cout << space_cnt << std::endl;
         }
     }
 
     //replace with %20
     if( space_cnt != 0) {
-        //work backwards so no afraid of overriding values
+        //work backwards so no afraid of overriding values when starting from the beginning
         int end = length + 2 * space_cnt; //for null char
         for(int i = length - 1; i >= 0 ; i--) {
             if(input[i] == ' ') {
-                input[i] = '%';
-                input[i+1] = '2';
-                input[i+2] = '0';
+                input[i + 2 * space_cnt] = '0';
+                input[i + 2 * space_cnt - 1] = '2';
+                input[i + 2 * space_cnt - 2] = '%';
+                space_cnt--;
             }
             else {
                 input[i + 2 * space_cnt] = input[i];
@@ -62,6 +51,5 @@ void insertPercent20(char* input, int length) {
 
         input[end] = '\0';
     }
-    std::cout << input << std::endl;
 
 }
